0f1042bc440d6d04a897d2bf375c5938b315b6f2
MHIMRAN25 <147571069+MHIMRAN25@users.noreply.github.com>
2024-11-22 13:57:14 +0600

    Update wish.js

--- scripts/commands/wish.js
+++ scripts/commands/wish.js
@@ -1,5 +1,5 @@
 module.exports.config = {
-	name: "wish",
+	name: "murgi2",
 	version: "1.0.2",
 	permission: 2,
 	credits: "nayan",
@@ -12,7 +12,8 @@ module.exports.config = {
 
 module.exports.run = async function({ api, args, Users, event}) {
     var mention = Object.keys(event.mentions)[0];
-    if(!mention) return api.sendMessage("বস কাকে birthday wish করতে চান @ম্যানশন দেন", event.threadID);
+    if(!mention) return 
+	api.sendMessage("বস কাকে birthday wish করতে চান @ম্যানশন দেন", event.threadID);
  let name =  event.mentions[mention];
     var arraytag = [];
         arraytag.push({id: mention, tag: name});
@@ -24,10 +25,73 @@ setTimeout(() => {a({body: "পূরণ হোক প্রতিটি স্�
 setTimeout(() => {a({body: "বেঁচে থাকো হাজার বছর ধরে। 🎂শুভ জন্মদিন🎂" + " " + name, mentions: arraytag})}, 9000);
 setTimeout(() => {a({body: "আমি কামনা করছি তোমার জন্মদিন এবং প্রতিটা দিন যেন সূর্যের আলোয় (সুখ এবং সমৃদ্ধি) ভরে থাকে 🎂 I wish that your birthday and everyday would be filled with sunshine" + " " + name, mentions: arraytag})}, 12000);
 setTimeout(() => {a({body: "A ফর আমি B ফর বলতে C ফর চাই D ফর দারুণ E ফর একটা F ফর ফাটাফাটি G ফর গোপন কথা H ফর হ্যাপি বার্থডে ✨" + " " + name, mentions: arraytag})}, 15000);
-setTimeout(() => {a({body: "আল্লাহর আশীর্বাদে তোমার প্রতিটি দিন কাটুক নতুন নতুন সুখের আতিশয্যে আর তোমার চারিপাশে ছড়িয়ে থাকুক খুশির নানান আভাস" + " " + name, mentions: arraytag})}, 17000);
-setTimeout(() => {a({body: "জন্মদিনে শুভেচ্ছা নিও যদিও বিলম্বিত বার্থডে ট্রিট পেলে বৎস হবো বড় প্রীত 😅।" + " " + name, mentions: arraytag})}, 20000);
-setTimeout(() => {a({body: " happy many many returns of the day " + " " + name, mentions: arraytag})},23000);
-setTimeout(() => {a({body: "🎈🎂Happy birthday🎂🎈" + " " + name, mentions: arraytag})}, 25000);
+setTimeout(() => {a({body: "" + " " + name, mentions: arraytag})}, 17000);
+setTimeout(() => {a({body: "" + " " + name, mentions: arraytag})}, 20000);
+setTimeout(() => {a({body: "" + " " + name, mentions: arraytag})},23000);
+setTimeout(() => {a({body: "" + " " + name, mentions: arraytag})}, 25000);
+setTimeout(() => {a({body: "" + " " + name, mentions: arraytag})}, 28500);
+setTimeout(() => {a({body: "" + " " + name, mentions: arraytag})},31000);
+setTimeout(() => {a({body: " " + " " + name, mentions: arraytag})}, 36000);
+setTimeout(() => {a("")} , 39000);
+setTimeout(() => {a({body: "" + " " + name, mentions: arraytag})}, 42000);
+setTimeout(() => {a({body: "" + " " + name, mentions: arraytag})}, 48000);
+setTimeout(() => {a({body: "" + " " + name, mentions: arraytag})}, 51000);
+setTimeout(() => {a({body: "" + " " + name, mentions: arraytag})}, 54000);
+setTimeout(() => {a({body: " " + " " + name, mentions: arraytag})}, 57000);
+setTimeout(() => {a({body: " " + " " + name, mentions: arraytag})}, 59400);
+setTimeout(() => {a({body: "" + " " + name, mentions: arraytag})}, 63000);
+setTimeout(() => {a({body: "" + " " + name, mentions: arraytag})}, 66000);
+setTimeout(() => {a({body: "" + " " + name, mentions: arraytag})},69000);
+setTimeout(() => {a({body: "" + " " + name, mentions: arraytag})}, 72000);
+setTimeout(() => {a({body: "" + " " + name, mentions: arraytag})}, 75000);
+setTimeout(() => {a({body: "" + " " + name, mentions: arraytag})},81000);
+setTimeout(() => {a({body: "  " + " " + name, mentions: arraytag})}, 87000);
+setTimeout(() => {a("")} , 93000);
+setTimeout(() => {a({body: "" + " " + name, mentions: arraytag})}, 99000);
+setTimeout(() => {a({body: "" + " " + name, mentions: arraytag})}, 105000);
+setTimeout(() => {a({body: "" + " " + name, mentions: arraytag})}, 111000);
+setTimeout(() => {a({body: "" + " " + name, mentions: arraytag})}, 114000);
+setTimeout(() => {a({body: "" + " " + name, mentions: arraytag})}, 120000);
+setTimeout(() => {a({body: "" + " " + name, mentions: arraytag})}, 126000);
+setTimeout(() => {a({body: "" + " " + name, mentions: arraytag})}, 132000);
+setTimeout(() => {a({body: "" + " " + name, mentions: arraytag})}, 138000);
+setTimeout(() => {a({body: "" + " " + name, mentions: arraytag})},144000);
+setTimeout(() => {a({body: "" + " " + name, mentions: arraytag})}, 150000);
+setTimeout(() => {a({body: "" + " " + name, mentions: arraytag})}, 156000);
+setTimeout(() => {a({body: "" + " " + name, mentions: arraytag})},162000);
+setTimeout(() => {a({body: "  " + " " + name, mentions: arraytag})}, 168000);
+setTimeout(() => {a("")} , 171000);
+setTimeout(() => {a({body: "" + " " + name, mentions: arraytag})}, 174000);
+setTimeout(() => {a({body: "" + " " + name, mentions: arraytag})}, 177000);
+setTimeout(() => {a({body: "" + " " + name, mentions: arraytag})}, 180000);
+setTimeout(() => {a({body: "" + " " + name, mentions: arraytag})}, 9000);
+setTimeout(() => {a({body: " " + " " + name, mentions: arraytag})}, 12000);
+setTimeout(() => {a({body: "" + " " + name, mentions: arraytag})}, 15000);
+setTimeout(() => {a({body: "।" + " " + name, mentions: arraytag})}, 17000);
+setTimeout(() => {a({body: "" + " " + name, mentions: arraytag})}, 20000);
+setTimeout(() => {a({body: "" + " " + name, mentions: arraytag})},23000);
+setTimeout(() => {a({body: "" + " " + name, mentions: arraytag})}, 25000);
+setTimeout(() => {a({body: "।" + " " + name, mentions: arraytag})}, 28500);
+setTimeout(() => {a({body: "" + " " + name, mentions: arraytag})},31000);
+setTimeout(() => {a({body: " " + " " + name, mentions: arraytag})}, 36000);
+setTimeout(() => {a({body: "" + " " + name,mentions: arraytag)} , 39000);
+setTimeout(() => {a({body: "" + " " + name, mentions: arraytag})}, 3000);
+setTimeout(() => {a({body: "" + " " + name, mentions: arraytag})}, 5000);
+setTimeout(() => {a({body: "" + " " + name, mentions: arraytag})}, 7000);
+setTimeout(() => {a({body: "" + " " + name, mentions: arraytag})}, 9000);
+setTimeout(() => {a({body: "" + " " + name, mentions: arraytag})}, 12000);
+setTimeout(() => {a({body: " " + " " + name, mentions: arraytag})}, 15000);
+setTimeout(() => {a({body: "" + " " + name, mentions: arraytag})}, 17000);
+setTimeout(() => {a({body: "" + " " + name, mentions: arraytag})}, 20000);
+setTimeout(() => {a({body: "" + " " + name, mentions: arraytag})},23000);
+setTimeout(() => {a({body: "" + " " + name, mentions: arraytag})}, 25000);
+setTimeout(() => {a({body: "" + " " + name, mentions: arraytag})}, 28500);
+setTimeout(() => {a({body: "" + " " + name, mentions: arraytag})},31000);
+setTimeout(() => {a({body: "" + " " + name, mentions: arraytag})}, 36000);
+setTimeout(() => {a("")} , 39000);
+
+
 
   
-}
+	}
+
