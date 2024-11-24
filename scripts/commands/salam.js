ecd2ecc33d85dcbc0ff33e19889e1f606673032e
MHIMRAN25 <147571069+MHIMRAN25@users.noreply.github.com>
2024-11-24 18:34:18 +0600

    Create salam.js

---
+++ scripts/commands/salam.js
@@ -0,0 +1,34 @@
+module.exports.config = {
+    name: "salam",
+  version: "1.0.0",
+  permission: 0,
+  credits: "Nayan",
+  description: "msg",
+  prefix: true, 
+  category: "kawsar", 
+  usages: "",
+  cooldowns: 5,
+  dependencies: {
+	}
+};
+module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
+	var { threadID, mesওsageID } = event;
+	let react = event.body.toLowerCase();
+	if(react.includes("السلام عليكم") ||
+     react.includes("السلام عليكم و الرحمة") || 
+react.includes("assalamu alaikum") ||
+react.includes("assalamu alaykum") ||
+react.includes("আসসালামু আলাইকুম") ||
+react.includes("আসসালামু আলাইকুম ওয়া রহমাতুল্লাহি") ||
+react.includes("আসসালামু আলাইকুম ওয়া রহমাতুল্লাহি ওয়া বারাকাতুহ") ||
+react.includes("🥰")) {
+		var msg = {
+				body: " ~ وعليكم السلام ورحمه الله وبركاته🥰!🌚"
+			}
+			api.sendMessage(msg, threadID, messageID);
+    api.setMessageReaction("🥰", event.messageID, (err) => {}, true)
+		}
+	}
+	module.exports.run = function({ api, event, client, __GLOBAL }) {
+
+  }
